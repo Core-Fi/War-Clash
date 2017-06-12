ee25dd258259dcac235fbf2faa3b04867674be48
zhaocy1217 <zcyandy@outlook.com>
2017-06-12 15:57:28 +0800

    无

---
+++ WarClash/Assets/Editor/SaveBoneVertex.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(Test_06_11))]
+public class SaveBoneVertex : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Generate"))
+        {
+            VAData va = new VAData();
+            var t = target as Test_06_11;
+            var smr = t.GetComponent<SkinnedMeshRenderer>();
+            var mesh = smr.sharedMesh;
+            Matrix4x4 l2w =  t.transform.localToWorldMatrix;
+            for (int i = 0; i < mesh.boneWeights.Length; i++)
+            {
+                var weight = mesh.boneWeights[i];
+                if (!va.dic.ContainsKey(smr.bones[weight.boneIndex0].name))
+                {
+                    va.dic[smr.bones[weight.boneIndex0].name] = new List<int>();
+                }
+                va.dic[smr.bones[weight.boneIndex0].name].Add(i);
+                if (!va.offset.ContainsKey(smr.bones[weight.boneIndex0].name))
+                {
+                    va.offset[smr.bones[weight.boneIndex0].name] = new List<VAData.V>();
+                }
+                Vector3 offset = smr.bones[weight.boneIndex0].transform.worldToLocalMatrix.MultiplyPoint(l2w.MultiplyPoint(mesh.vertices[i]));
+                va.offset[smr.bones[weight.boneIndex0].name].Add(new VAData.V() {x = offset.x, y = offset.y, z = offset.z });
+            }
+            string j = Newtonsoft.Json.JsonConvert.SerializeObject(va);
+            File.WriteAllText(Application.dataPath + "/" + t.gameObject.name + ".json", j);
+        }
+        base.OnInspectorGUI();
+    }
+}
--- WarClash/Assets/Test_06_11.cs
+++ WarClash/Assets/Test_06_11.cs
@@ -1,35 +1,84 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 public class Test_06_11 : MonoBehaviour
 {
 
-    public SkinnedMeshRenderer mf;
-
-    public Dictionary<int, List<int>> dic = new Dictionary<int, List<int>>();
-
+    private Mesh mesh;
+    private Dictionary<Transform, List<int>> t_l = new Dictionary<Transform, List<int>>();
+    private Dictionary<Transform, List<Vector3>> t_o = new Dictionary<Transform, List<Vector3>>();
+    private Matrix4x4 world2localM;
     // Use this for initialization
     void Start()
     {
-
-        var mesh = mf.sharedMesh;
-        for (int i = 0; i < mesh.boneWeights.Length; i++)
+        world2localM = transform.worldToLocalMatrix;
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertexs = new Vector3[mesh.vertices.Length];
+        string str = File.ReadAllText(Application.dataPath + "/" + gameObject.name + ".json");
+        VAData o = Newtonsoft.Json.JsonConvert.DeserializeObject<VAData>(str);
+        foreach (var item in o.dic)
         {
-            var weight = mesh.boneWeights[i];
-            if (!dic.ContainsKey(weight.boneIndex0))
+            Transform t = GetChild(transform.parent, item.Key);
+            if(t != null)
             {
-                dic[weight.boneIndex0] = new List<int>();
+                t_l[t] = item.Value;
             }
-            dic[weight.boneIndex0].Add(i);
+            t_o[t] = new List<Vector3>();
+            for (int i = 0; i < item.Value.Count; i++)
+            {
+                VAData.V v =  o.offset[item.Key][i];
+                t_o[t].Add(new Vector3(v.x, v.y, v.z));
+            }
+       
         }
+    }
+    public Transform GetChild(Transform parent, string name)
+    {
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            var t = parent.GetChild(i);
+            Transform target;
+            if (t.name == name)
+            {
+                return t;
+            }
+            else
+            {
+                target = GetChild(t, name);
+            }
+            if(target!= null)
+            {
+                return target;
+            }
+        }
+        return null;
 
     }
-
+    Vector3[] vertexs;
     // Update is called once per frame
     void Update()
     {
+        foreach (var item in t_l)
+        {
+            for (int i = 0; i < item.Value.Count; i++)
+            {
+                vertexs[item.Value[i]] = world2localM.MultiplyPoint(item.Key.localToWorldMatrix.MultiplyPoint(t_o[item.Key][i]));
+            }
+        }
 
-
-
+        mesh.vertices = vertexs;
+  //      mesh.RecalculateNormals();
     }
+}
+
+public class VAData
+{
+    public struct V
+    {
+        public float x, y, z;
+    }
+    public Dictionary<string, List<int>> dic = new Dictionary<string, List<int>>();
+    public Dictionary<string, List<V>> offset = new Dictionary<string, List<V>>();
 }
